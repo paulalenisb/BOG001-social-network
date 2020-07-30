2b800fc0d4b8130cbac0dc8ded897c368c0d76f7
AnaRativa09 <anarativa09@gmail.com>
2020-07-29 19:04:24 -0500

    Config routes + firebase

--- src/main.js
+++ src/main.js
@@ -4,16 +4,13 @@
 
 // myFunction();
 
-  
 import { router } from "./router/index.routes.js";
 import "./main.scss";
 
 router(window.location.hash);
 window.addEventListener("hashchange", () => {
-   
-    router(window.location.hash);
-   
-  });
+  router(window.location.hash);
+});
 // };
 
 // window.addEventListener("load", init);
