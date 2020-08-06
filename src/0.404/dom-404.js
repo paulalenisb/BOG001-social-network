2c7350ef48ca17ee218349d4663e79cc278cbde7
Paula Lenis B <paulalenisb@gmail.com>
2020-08-06 09:25:34 -0500

    Post con firebase

--- src/0.404/dom-404.js
+++ src/0.404/dom-404.js
@@ -1,14 +1,39 @@
 import view from './404.html';
+import '../firebase/firebaseConfig';
 import './estilos-404.css';
+import { dataBase } from '../Firebase/firebaseStore'
 
 export default () => {
+
   const divElement = document.createElement('div');
   divElement.innerHTML = view;
 
-  const btnClick = divElement.querySelector('#btnClick');
-  btnClick.addEventListener('click', () => {
-    alert('click');
+  // const btnClick = divElement.querySelector('#btnClick');
+  // btnClick.addEventListener('click', () => {
+  //   alert('click');
+  // });
+
+  const savePost = (postTitle, postDescription) =>
+  dataBase.collection("recomendaciones").doc().set({
+    postTitle,
+    postDescription,
   });
+
+  const postForm = divElement.querySelector('#post-form');
+
+  postForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    //Capturar el nombre del restaurante
+    const postTitle = postForm['post-title'].value;
+    const postDescription = postForm['post-description'].value;
+
+    await savePost(postTitle,postDescription);
+    postForm.reset();
+
+  })
 
   return divElement;
 };
+
+//min 25 https://www.youtube.com/watch?v=itNsRn1kjLU&t=1s
