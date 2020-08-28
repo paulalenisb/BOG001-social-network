7e9d486006c34362d66c40604adfa31ddb230399
AnaRativa09 <anarativa09@gmail.com>
2020-08-28 18:01:24 -0500

    Footer con SVG + Intento botones active por vista

--- src/main.js
+++ src/main.js
@@ -8,15 +8,55 @@ window.addEventListener('hashchange', () => {
   router(window.location.hash)
 });
 
-/* ------ Button active -------*/
-/* const header = document.getElementById("nav-desktop");
-const btns = header.getElementsByClassName("btn");
-console.log(btns);
 
-for (let i = 0; i < btns.length; i++) {
+
+/* ------ Button active -------*/
+
+/* const routerNav = (route) => {
+  switch (route) {
+    case '#/home':
+      window.location = "#/home"
+      break;
+    case '#/filtro':
+      window.location = "#/filtro"
+      break;
+    case '#/post':
+      window.location = "#/post"
+      break;
+    case '#/profile':
+      window.location = "#/profile";
+      break;
+    default:
+      window.location = "#/home";
+  }
+}; */ 
+
+/*const header = document.getElementById("nav-desktop");
+const btns = header.getElementsByClassName("btn");
+console.log(btns);*/
+
+/*for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-  const current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    console.log(this.value);
+
+    /* routerNav(this.value) */
+    /* router(this.value) */
+    
+  /*   if(this.value == "#/home"){
+      window.location = "#/home"
+    } */
+    /* if(this.value == "#/filtro"){
+      window.location = "#/filtro"
+    }
+    if(this.value == "#/post"){
+      window.location = "#/post"
+    }
+    if(this.value == "#/profile"){
+      window.location = "#/profile"
+    } 
   });
-} */
+}*/
+
