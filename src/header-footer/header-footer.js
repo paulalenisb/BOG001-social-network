3a4020087f3ffc1adfd5300d73080525667027e3
AnaRativa09 <anarativa09@gmail.com>
2020-08-28 22:09:21 -0500

    Agregando clase activa en nav para cada vista

--- src/header-footer/header-footer.js
+++ src/header-footer/header-footer.js
@@ -28,7 +28,7 @@ export const headerTemplate = () => {
         <ul>
           <li class="nav-item-mobile">
             <a href="#/home">
-              <svg id="icon-home-desk" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><path id="HOME" class="cls-1" d="M96.14,43.66h0L56.35,3.86a9,9,0,0,0-12.7,0L3.89,43.63l0,0a9,9,0,0,0,6,15.3h1.86V88.26A10.53,10.53,0,0,0,22.19,98.77H37.76a2.86,2.86,0,0,0,2.86-2.86V73a4.8,4.8,0,0,1,4.79-4.79h9.18A4.81,4.81,0,0,1,59.39,73v23a2.85,2.85,0,0,0,2.85,2.86H77.81A10.53,10.53,0,0,0,88.32,88.26V59h1.47a9,9,0,0,0,6.35-15.32Z"/></svg>
+              <svg id="icon-home-desk" class="btn active" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><path id="HOME" class="cls-1" d="M96.14,43.66h0L56.35,3.86a9,9,0,0,0-12.7,0L3.89,43.63l0,0a9,9,0,0,0,6,15.3h1.86V88.26A10.53,10.53,0,0,0,22.19,98.77H37.76a2.86,2.86,0,0,0,2.86-2.86V73a4.8,4.8,0,0,1,4.79-4.79h9.18A4.81,4.81,0,0,1,59.39,73v23a2.85,2.85,0,0,0,2.85,2.86H77.81A10.53,10.53,0,0,0,88.32,88.26V59h1.47a9,9,0,0,0,6.35-15.32Z"/></svg>
             </a>
             <p>Home</p>
           </li>
@@ -77,25 +77,25 @@ export const footerTemplate = () => {
         <ul>
           <li class="nav-item-mobile">
             <a href="#/home">
-              <svg id="icon-home-desk" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><path id="HOME" class="cls-1" d="M96.14,43.66h0L56.35,3.86a9,9,0,0,0-12.7,0L3.89,43.63l0,0a9,9,0,0,0,6,15.3h1.86V88.26A10.53,10.53,0,0,0,22.19,98.77H37.76a2.86,2.86,0,0,0,2.86-2.86V73a4.8,4.8,0,0,1,4.79-4.79h9.18A4.81,4.81,0,0,1,59.39,73v23a2.85,2.85,0,0,0,2.85,2.86H77.81A10.53,10.53,0,0,0,88.32,88.26V59h1.47a9,9,0,0,0,6.35-15.32Z"/></svg>
+              <svg id="icon-home-mob" class="btn active" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><path id="HOME" class="cls-1" d="M96.14,43.66h0L56.35,3.86a9,9,0,0,0-12.7,0L3.89,43.63l0,0a9,9,0,0,0,6,15.3h1.86V88.26A10.53,10.53,0,0,0,22.19,98.77H37.76a2.86,2.86,0,0,0,2.86-2.86V73a4.8,4.8,0,0,1,4.79-4.79h9.18A4.81,4.81,0,0,1,59.39,73v23a2.85,2.85,0,0,0,2.85,2.86H77.81A10.53,10.53,0,0,0,88.32,88.26V59h1.47a9,9,0,0,0,6.35-15.32Z"/></svg>
             </a>
             <p>Home</p>
           </li>
           <li class="nav-item-mobile">
             <a href="#/filtro">
-            <svg id="icon-filter-desk" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="FILTROS"><path class="cls-1" d="M94.8,13.62H36.89a11.94,11.94,0,0,0-22.65,0h-9a3.8,3.8,0,0,0,0,7.6h9a11.94,11.94,0,0,0,22.65,0H94.8a3.8,3.8,0,0,0,0-7.6Z"/><path class="cls-1" d="M94.8,46.2h-9a11.94,11.94,0,0,0-22.65,0H5.2a3.8,3.8,0,0,0,0,7.6H63.11a11.94,11.94,0,0,0,22.65,0h9a3.8,3.8,0,0,0,0-7.6Z"/><path class="cls-1" d="M94.8,78.78H53.18a11.94,11.94,0,0,0-22.65,0H5.2a3.8,3.8,0,1,0,0,7.6H30.53a11.94,11.94,0,0,0,22.65,0H94.8a3.8,3.8,0,0,0,0-7.6Z"/></g></svg>
+            <svg id="icon-filter-mob" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="FILTROS"><path class="cls-1" d="M94.8,13.62H36.89a11.94,11.94,0,0,0-22.65,0h-9a3.8,3.8,0,0,0,0,7.6h9a11.94,11.94,0,0,0,22.65,0H94.8a3.8,3.8,0,0,0,0-7.6Z"/><path class="cls-1" d="M94.8,46.2h-9a11.94,11.94,0,0,0-22.65,0H5.2a3.8,3.8,0,0,0,0,7.6H63.11a11.94,11.94,0,0,0,22.65,0h9a3.8,3.8,0,0,0,0-7.6Z"/><path class="cls-1" d="M94.8,78.78H53.18a11.94,11.94,0,0,0-22.65,0H5.2a3.8,3.8,0,1,0,0,7.6H30.53a11.94,11.94,0,0,0,22.65,0H94.8a3.8,3.8,0,0,0,0-7.6Z"/></g></svg>
             </a>
             <p>Filtro</p>
           </li>
           <li class="nav-item-mobile">
             <a href="#/post">
-            <svg id="icon-post-desk" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="FORK"><polygon class="cls-1" points="53.78 98.19 53.78 98.19 53.78 98.17 53.78 98.17 53.78 98.19"/><path class="cls-1" d="M98.29,50.07A48.29,48.29,0,1,0,47,98.21l0-26a2.12,2.12,0,0,0-1.5-2A16.84,16.84,0,0,1,33.62,54.11V27.22c0-2.68,3.15-4.64,6-2.34a2.09,2.09,0,0,1,.76,1.63V48.62a2.13,2.13,0,0,0,2.13,2.13h2.45a2.13,2.13,0,0,0,2.14-2.13V27.22c0-2.68,3.16-4.64,6-2.34a2.12,2.12,0,0,1,.75,1.64v22.1a2.13,2.13,0,0,0,2.14,2.13h2.45a2.13,2.13,0,0,0,2.13-2.13V26.51a2.09,2.09,0,0,1,.76-1.63c2.81-2.3,6-.34,6,2.34V54.11a16.84,16.84,0,0,1-12,16.09,2.12,2.12,0,0,0-1.5,2V98.17A48.33,48.33,0,0,0,98.29,50.07Z"/></g></svg>
+            <svg id="icon-post-mob" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="FORK"><polygon class="cls-1" points="53.78 98.19 53.78 98.19 53.78 98.17 53.78 98.17 53.78 98.19"/><path class="cls-1" d="M98.29,50.07A48.29,48.29,0,1,0,47,98.21l0-26a2.12,2.12,0,0,0-1.5-2A16.84,16.84,0,0,1,33.62,54.11V27.22c0-2.68,3.15-4.64,6-2.34a2.09,2.09,0,0,1,.76,1.63V48.62a2.13,2.13,0,0,0,2.13,2.13h2.45a2.13,2.13,0,0,0,2.14-2.13V27.22c0-2.68,3.16-4.64,6-2.34a2.12,2.12,0,0,1,.75,1.64v22.1a2.13,2.13,0,0,0,2.14,2.13h2.45a2.13,2.13,0,0,0,2.13-2.13V26.51a2.09,2.09,0,0,1,.76-1.63c2.81-2.3,6-.34,6,2.34V54.11a16.84,16.84,0,0,1-12,16.09,2.12,2.12,0,0,0-1.5,2V98.17A48.33,48.33,0,0,0,98.29,50.07Z"/></g></svg>
             </a>
             <p>Reseña</p>
           </li>
           <li class="nav-item-mobile">
             <a href="#/profile">
-            <svg id="icon-user-desk" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="PERFIL"><path class="cls-1" d="M98.6,98.6A48.65,48.65,0,0,0,65.9,52.65a28.1,28.1,0,1,0-31.8,0A48.65,48.65,0,0,0,1.4,98.6Z"/></g></svg>
+            <svg id="icon-user-mob" class="btn" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.cls-1{stroke-miterlimit:10;}</style></defs><g id="PERFIL"><path class="cls-1" d="M98.6,98.6A48.65,48.65,0,0,0,65.9,52.65a28.1,28.1,0,1,0-31.8,0A48.65,48.65,0,0,0,1.4,98.6Z"/></g></svg>
             </a>
             <p>Perfil</p>
           </li>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,58 @@
-// Este es el punto de entrada de tu aplicacion
-
 import { router } from './router/index.routes.js';
 import './main.scss';
 
+/*------ Router views -------*/
 router(window.location.hash);
 window.addEventListener('hashchange', () => {
   router(window.location.hash)
 });
 
+window.addEventListener('hashchange', () => {
+  /*------ Button active mobile -------*/
+  const iconHomeMob = document.querySelector('#icon-home-mob');
+  const iconFilterMob = document.querySelector('#icon-filter-mob');
+  const iconPostMob = document.querySelector('#icon-post-mob');
+  const iconProfileMob = document.querySelector('#icon-user-mob');
+  
+  if(window.location.hash == '#/home'){
+    iconHomeMob.classList.add('active')
+  } 
+  if(window.location.hash == '#/filtro'){
+    iconFilterMob.classList.add('active')
+    iconHomeMob.classList.remove('active')
+  } 
+  if(window.location.hash == '#/post'){
+    iconPostMob.classList.add('active')
+    iconHomeMob.classList.remove('active')
+  } 
+  if(window.location.hash == '#/profile'){
+    iconProfileMob.classList.add('active')
+    iconHomeMob.classList.remove('active')
+  } 
 
+  /*------ Button active desktop -------*/
+  const iconHome = document.querySelector('#icon-home-desk');
+  const iconFilter = document.querySelector('#icon-filter-desk');
+  const iconPost = document.querySelector('#icon-post-desk');
+  const iconProfile = document.querySelector('#icon-user-desk');
+  
+
+  if(window.location.hash == '#/home'){
+    iconHome.classList.add('active')
+  } 
+  if(window.location.hash == '#/filtro'){
+    iconFilter.classList.add('active')
+    iconHome.classList.remove('active')
+  } 
+  if(window.location.hash == '#/post'){
+    iconPost.classList.add('active')
+    iconHome.classList.remove('active')
+  } 
+  if(window.location.hash == '#/profile'){
+    iconProfile.classList.add('active')
+    iconHome.classList.remove('active')
+  } 
+});
 
 /* ------ Button active -------*/
 
@@ -31,16 +75,16 @@ window.addEventListener('hashchange', () => {
   }
 }; */ 
 
-/*const header = document.getElementById("nav-desktop");
+/* const header = document.getElementById("nav-desktop");
 const btns = header.getElementsByClassName("btn");
-console.log(btns);*/
+console.log(btns);
 
-/*for (let i = 0; i < btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     const current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
-    console.log(this.value);
+    console.log(this.value); */
 
     /* routerNav(this.value) */
     /* router(this.value) */
@@ -56,7 +100,9 @@ console.log(btns);*/
     }
     if(this.value == "#/profile"){
       window.location = "#/profile"
-    } 
-  });
-}*/
+    } */
+
+
+/*   });
+} */
 
